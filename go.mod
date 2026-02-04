d2c63904c36aed9e101ba7a648d53dcc3b0eb855
PavelMenshikov <creepskip2@gmail.com>
2026-02-04 14:17:03 +0300

    chore: switch to goose migrator and fix sql syntax

--- go.mod
+++ go.mod
@@ -38,9 +38,14 @@ require (
 	github.com/go-openapi/swag v0.19.15 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/pressly/goose/v3 v3.26.0 // indirect
+	github.com/sethvargo/go-retry v0.3.0 // indirect
 	github.com/swaggo/files v0.0.0-20220610200504-28940afbdbfe // indirect
+	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/mod v0.17.0 // indirect
 	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- migrations/000002_create_courses_structure.sql
+++ migrations/000002_create_courses_structure.sql
@@ -1,7 +1,3 @@
-CREATE TYPE course_status AS ENUM ('active', 'completed', 'archived');
-CREATE TYPE assignment_status AS ENUM ('pending_check', 'rejected', 'accepted');
-
-
 CREATE TABLE IF NOT EXISTS teachers (
     id UUID PRIMARY KEY,
     bio TEXT,
@@ -11,27 +7,24 @@ CREATE TABLE IF NOT EXISTS teachers (
     FOREIGN KEY (id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-
 CREATE TABLE IF NOT EXISTS courses (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     title VARCHAR(255) NOT NULL,
     description TEXT,
     image_url VARCHAR(255),
     is_main BOOLEAN NOT NULL DEFAULT FALSE, 
-    status course_status NOT NULL DEFAULT 'active',
+    status VARCHAR(20) NOT NULL DEFAULT 'draft',
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
 
- Модулей
 CREATE TABLE IF NOT EXISTS modules (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     course_id UUID NOT NULL,
     title VARCHAR(255) NOT NULL,
     order_num INTEGER NOT NULL,
-    FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE,
-    UNIQUE (course_id, order_num)
+    description TEXT,
+    FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE
 );
-
 
 CREATE TABLE IF NOT EXISTS lessons (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
